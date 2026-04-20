9bb6e4821d90021c2e22f4448e19a9d2a46686bb
Raya aka Sam <rayacoding90@gmail.com>
2026-04-20 13:07:37 +0800

    edit sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // next-sitemap.config.js
 module.exports = {
-  siteUrl: 'https://nexuz-unity.vercel.app', // Ganti dengan URL Vercel kamu!
+  siteUrl: 'https://nexuz-unity.vercel.app',
   generateRobotsTxt: true,
-  exclude: ['/admin/*', '/api/*'], // Jangan sertakan halaman admin di sitemap
+  exclude: ['/admin', '/admin/*', '/api/*'],
 };
